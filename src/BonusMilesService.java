67e408895890db2f2636e69a1b7b07b757d8e804
arthur.salahutdinov <artes_93@mail.ru>
2024-09-02 20:42:56 +0300

    Добавлены изменения в Main.java и BonusMilesService.java

--- src/BonusMilesService.java
+++ src/BonusMilesService.java
@@ -1,4 +1,7 @@
-package PACKAGE_NAME;
-
 public class BonusMilesService {
+    public int calculate(int price) {
+        int milesPerRuble = 20; // количество рублей для одной мили
+        int miles = price / milesPerRuble; // расчёт количества миль
+        return miles;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,8 @@
-package PACKAGE_NAME;
-
 public class Main {
+    public static void main(String[] args) {
+        BonusMilesService service = new BonusMilesService();
+        int price = 12_500;
+        int miles = service.calculate(price); // должно получиться 500
+        System.out.println(miles);
+    }
 }
